8f1aca57c9adbfa152c25374f1ceb0a5177109d5
epsminhtri@gmail.com <epsminhtri@gmail.com>
2018-07-25 15:20:42 +0700

    ko

---
+++ wp-content/themes/T-theme/page-contact.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+    <div class="container">
+        <div class="title-contact">
+            <div class="linkien_title row" >
+                <p class = " wow fadeIn" data-wow-duration=".3" data-wow-delay=".5s">Liên hệ</p>
+            </div>
+        </div>
+        <div class="content-contact">
+            <div class="row">
+                <div class="col-md-6 contact-form">
+                    <form action="">
+                        <div class="row msg-contact"><p>Vui lòng điền vào Form liên hệ và gửi.</p></div>
+                        <div class="row form-group">
+                            <div class="col-md-4 title-form"><b>Họ tên:</b></div>
+                            <div class="col-md-8 input-form"><input type="text" class = "form-control"></div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-4 title-form"><b>Email:</b></div>
+                            <div class="col-md-8 input-form"><input type="text" class = "form-control"></div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-4 title-form"><b>Số điện thoại:</b></div>
+                            <div class="col-md-8 input-form"><input type="text" class = "form-control"></div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-4 title-form"><b>Nội dung liên hệ:</b></div>
+                            <div class="col-md-8 input-form">
+                                <textarea name="" id="" cols="50" rows="10" class = "form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="row form-group submit-form">
+                            <button class = "btn btn-primary" type="submit"><b>Gửi</b></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+<?php get_footer(); ?>
